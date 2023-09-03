96a2f3eba6cb5af52280950aa2b633ded99c026b
thunder2367shiv <2022kuec2034@iiitkota.ac.in>
2023-09-03 23:20:59 +0530

    first commit

---
+++ myjs.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['Do you known about Me ??', 'Explore my Profile', 'Known about My past Experiences', 'Explore and see my works and Services'],
+    typeSpeed: 70,
+  });
